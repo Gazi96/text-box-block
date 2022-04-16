5bf87a8466d8917b2d9500ad2ac0ef92feca9b31
Gazi96 <rafalgazda.mail@gmail.com>
2022-04-16 22:40:13 +0200

    Init project

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1e38b8ea60f21044673e98dc656f111a');
